f1fa20abbee129f9911bd455420e41f5a355e5b2
Le Quang Huy <huylq19@vpbank.com.vn>
2024-07-31 12:54:41 +0700

    update

--- example/ios/Runner/AppDelegate.swift
+++ example/ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import Flutter
 import UIKit
+import flutter_linkid_gami
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,6 +9,7 @@ import UIKit
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+      LinkIDGameSDK.shared.setHostMainWindow(window)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
